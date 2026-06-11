594739bbf34a187125921acb0204c6fd5f0a9316
joshivanshita269 <joshivanshita269@gmail.com>
2026-06-11 10:26:22 +0530

    Add files via upload

--- u1_p10.php
+++ u1_p10.php
@@ -3,18 +3,18 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Program 10</title>
 </head>
 <body>
     <?php
-        $may_salary=array(35000,40000,45000);
-        $june_salary=array(35000,50000,45000);
-
-        $s=array_intersect($may_salary,$june_salary);
-        foreach($s as $m)
+        echo "Merge of Arrays";
+        $ar1=array(10,23,45,75);
+        $ar2=array(10,75,34,23);
+        $mer=array_intersect($ar1,$ar2);
+        foreach($mer as $m)
         {
             echo $m."<br>";
         }
     ?>
-        </body>
+</body>
 </html>
